1a52d226543cf5dd8de22b7900d8311ed669f455
Hazieon <handrew3721@gmail.com>
2021-02-17 22:57:48 +0000

    page renders current coin price per cryptocurrency, plus basic css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
 import axios from "axios";
 import "./App.css";
 const key = process.env.KEY;
@@ -9,19 +8,51 @@ function App() {
   useEffect(() => {
     axios
       .get(
-        `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,ADA,GRT,LTC, &tsyms=BTC,USD,EUR,GBP&api_key=${key}`
+        `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,ADA,GRT,LTC,&tsyms=BTC,USD,EUR,GBP&api_key=${key}`
       )
       .then((res) => {
         const cryptos = res.data;
-        console.log(cryptos);
-        setCryptos(cryptos);
+        console.log(cryptos, "cryptos");
+        setCryptos(cryptos.DISPLAY);
       });
   }, []);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Welcome</h1>
+      {Object.keys(cryptos).map((key, i) => {
+        console.log(cryptos[key].GBP.IMAGEURL);
+        return (
+          <div id="crypto-container">
+            <span className="left">{key}</span>
+            <span className="right">{cryptos[key].GBP.PRICE}</span>
+            {/* <img src={cryptos[key].GBP.IMAGEURL} alt="coin logo" /> */}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
 
 //get prices only
 // "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=GBP"
+
+// {Object.keys(
+//   cryptos.length > 1
+//     ? cryptos.map((key) => {
+//         return (
+//           <div id="crypto-container">
+//             <span className="left">{key}</span>
+//             <span className="right">{cryptos.gbp}</span>
+//           </div>
+//         );
+//       })
+//     : ""
+// )}
+
+//  <div id="crypto-container">
+//             <span className="left">{key}</span>
+//             <span className="right">{"hello"}</span>
+//           </div>
