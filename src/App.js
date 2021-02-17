e5cd1e94a71b6730192639260ea4c06ca2833513
Hazieon <handrew3721@gmail.com>
2021-02-17 21:36:29 +0000

    API call set up in app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import axios from "axios";
+import "./App.css";
+const key = process.env.KEY;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [cryptos, setCryptos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,ADA,GRT,LTC, &tsyms=BTC,USD,EUR,GBP&api_key=${key}`
+      )
+      .then((res) => {
+        const cryptos = res.data;
+        console.log(cryptos);
+        setCryptos(cryptos);
+      });
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
+
+//get prices only
+// "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=GBP"
